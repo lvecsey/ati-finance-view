3afb6877d0472cede459de8d9abe28dab1dc81d0
Chess Now <cn-github@fractal-interval.com>
2010-10-27 06:45:48 -0400

    ensure root account for sshd has same matching owner for authorized_keys2

--- inject-sshd.sh
+++ inject-sshd.sh
@@ -9,12 +9,13 @@ fi
 
 echo Creating priv separation directory, otherwise sshd will not start.
 mkdir -p "$bd"/var/run/sshd
+chown root.root "$bd"/var/run/sshd
 
 echo Copying over authorized keys files.
 mkdir -p "$bd"/root/.ssh
 cp injections/authorized_keys2 "$bd"/root/.ssh
+chown root.root "$bd"/root/.ssh/authorized_keys2
 chmod 600 "$bd"/root/.ssh/authorized_keys2
 
 echo You need to go in and allow root for ssh access, in /etc/ssh/sshd_config
 grep PermitRoot "$bd"/etc/ssh/sshd_config
-
